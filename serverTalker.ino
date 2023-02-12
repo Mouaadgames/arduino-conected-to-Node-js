60bfb918eff7a6e852e414bc4b57ab2035bd4f6b
Mouaadgames <mouaadgames7@gmail.com>
2023-02-12 20:37:34 +0100

    working script

--- serverTalker.ino
+++ serverTalker.ino
@@ -4,17 +4,23 @@
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2, LCD_5x8DOTS);
 int state = 0;
+bool mainLightState = true;
 int lastState = -1;
 bool lastButtonState = false;
 bool buttonState = false;
 bool comunicationStarted = false;
 
+#define buzzer 8
+#define mainLight 3
 void setup()
 {
     pinMode(11, OUTPUT);
     pinMode(12, OUTPUT);
     pinMode(13, OUTPUT);
     pinMode(2, INPUT);
+    pinMode(buzzer, OUTPUT);
+    pinMode(mainLight, OUTPUT);
+    digitalWrite(mainLight, mainLightState);
     Serial.begin(9600);
     // initialize the LCD
     lcd.begin();
@@ -38,17 +44,39 @@ void loop()
             comunicationStarted = true;
             Serial.println("ok");
         }
+
         if (incomingString == "get")
         {
             Serial.println(state);
+            if (mainLightState)
+            {
+                Serial.println("on");
+            }
+            else
+            {
+                Serial.println("off");
+            }
+            return;
         }
 
         if (incomingString == "0" || incomingString == "1" || incomingString == "2")
         {
             state = incomingString.toInt();
-            Serial.println("Done: " + incomingString);
+            Serial.println("Done: " + incomingString); // I should change that
             return;
         }
+
+        if (incomingString == "toggleLight")
+        {
+            mainLightState = !mainLightState;
+            digitalWrite(mainLight, mainLightState);
+        }
+
+        if (incomingString == "buzzer")
+        {
+            tone(buzzer, 300, 400);
+        }
+
         Serial.println(incomingString);
     }
 
@@ -78,7 +106,6 @@ void loop()
             lcd.print("RED is showing");
             digitalWrite(13, HIGH);
         }
-        Serial.println("changed");
         Serial.println(state);
     }
 
